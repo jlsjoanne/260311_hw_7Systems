2ef6f3ef63975c95483285296f39eb3dc366f173
Joanne H <jlsjoanne@gmail.com>
2026-03-25 13:26:03 +0800

    feat: complete file download system, with FileList and File_Add page

---
+++ System03_File/FileList.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="檔案下載系統" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FileList.aspx.cs" Inherits="_260311_hw_7Systems.System03_File.FileList" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <div>
+            <asp:Button ID="AddNew" runat="server" Text="新增檔案" OnClick="AddNew_Click" Visible="False"/>
+        </div>
+        <div>
+            <asp:GridView ID="FileGrid" runat="server" DataKeyNames="FileId"
+                OnRowCommand="FileGrid_RowCommand" OnRowDeleting="FileGrid_RowDeleting" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:CommandField ShowDeleteButton="False" />
+                    <asp:BoundField HeaderText="分類" DataField="CategoryName"/>
+                    <asp:TemplateField HeaderText="檔案名稱">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="FileDownload" runat="server"
+                                Text='<%# Eval("FileTitle") %>'
+                                CommandName="Download"
+                                CommandArgument='<%# CombinePath("~/Files/", Eval("FilePath")) %>' ></asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:BoundField HeaderText="檔案說明" DataField="FileDesc" />
+                    <asp:BoundField HeaderText="上傳時間" DataField="UploadDate" />
+
+                </Columns>
+
+            </asp:GridView>
+        </div>
+    </main>
+</asp:Content>
---
+++ System03_File/File_Add.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace _260311_hw_7Systems.System03_File
+{
+    public partial class File_Add : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["RoleId"] != null && (Session["RoleId"].ToString() == "1" || Session["RoleId"].ToString() == "2"))
+            {
+
+            }
+            else if (Request.UrlReferrer != null)
+            {
+                Response.Redirect(Request.UrlReferrer.ToString());
+            }
+            else
+            {
+                Response.Redirect("FileList.aspx");
+            }
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["FilesDB"].ConnectionString;
+            string insertFileQuery = "INSERT INTO Files (FileTitle, FileDesc, FilePath, CategoryId) VALUES (@FileTitle, @FileDesc, @FilePath, @CategoryId);";
+            string fileName = FileName.Text;
+            if(fileName == "")
+            {
+                fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+            }
+
+            if (FileUpload1.HasFile)
+            {
+                string folderPath = Server.MapPath("~/Files/");
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                string savedFile = Guid.NewGuid().ToString() + Path.GetExtension(FileUpload1.FileName);
+                FileUpload1.SaveAs(Path.Combine(folderPath, savedFile));
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(insertFileQuery, conn))
+                    {
+                        command.Parameters.AddWithValue("@FileTitle", fileName);
+                        command.Parameters.AddWithValue("@FileDesc", FileDesc.Text);
+                        command.Parameters.AddWithValue("@FilePath", Path.Combine(folderPath, savedFile));
+                        command.Parameters.AddWithValue("@CategoryId", CategoryDropdownList.SelectedValue);
+
+                        try
+                        {
+                            conn.Open();
+                            int result = command.ExecuteNonQuery();
+                            if(result < 0)
+                            {
+                                Response.Write("<script>alert('檔案無法寫入資料庫')</script>");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Response.Write($"<script>alert('{ex.Message}')</script>");
+                        }
+                        finally
+                        {
+                            conn.Close();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('無選擇上傳檔案')</script>");
+                return;
+            }
+
+            Response.Redirect("FileList.aspx");
+        }
+    }
+}
